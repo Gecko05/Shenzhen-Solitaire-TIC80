5cec3927de4a6dd7194ab499f664058b70ede465
Jaime Centeno <jacacent@gmail.com>
2019-09-27 13:07:19 -0500

    Update shenzhen.lua

--- shenzhen.lua
+++ shenzhen.lua
@@ -3,13 +3,26 @@
 -- desc:   Solitaire from Shenzhen I/O
 -- script: lua
 -- input:  mouse
+ -- specials: 31 41 51
+piles = {{31,1,2,3,4},
+        {5,6,7,8,9},
+        {11,12,13,14},
+        {15,16,17,18,19},
+        {21,22,23,24,25},
+        {26,27,28,29},
+        {31,41,51},
+        {51,41,31}}
 
-piles = {{1,2,3,4,},
-        {5,6,7,8,9,},
-        {10,11,12,13,},
-        {14,15,16,17,18},
-        {19,20,21,22,23},
-        {24,25,26,27}}
+endpiles = {{},
+            {},
+            {},
+            {}
+            }
+			
+tokpiles = {{},
+            {},
+            {}
+            }
 drag = {}
 trans = {}
 hold = false
@@ -19,6 +32,9 @@ mx = 0
 my = 0
 lx = 0
 ly = 0
+btn_red = 178
+btn_blk = 180
+btn_grn = 182
 -- Draw a blank card
 function drawBlnkCard(x, y)
 	spr(0, x,  y,   2)
@@ -58,10 +74,13 @@ function isDifColor(num0, num1)
 end
 -- Check if 1st card is bigger than 2nd one
 function isOrdered(num0,num1)
- if isDifColor(num0,num1) then
-		n0 = num0 % 9
-		n1 = num1 % 9
-	 if n0 - n1 == 1 then
+	if num0 == nil then
+		return true
+	end
+	if isDifColor(num0,num1) then
+		n0 = num0 % 10
+		n1 = num1 % 10
+		if n0 - n1 == 1 then
 			return true
 		end
 	end
@@ -74,7 +93,7 @@ function isDraggable(c,r)
 	end
 	n = 1
 	while piles[c][r+n]~=nil do
-	 num = piles[c][r+n]
+		num = piles[c][r+n]
 		if isOrdered(piles[c][r+n-1],num)==false  then
 			return false
 		end
@@ -95,21 +114,81 @@ function drawCards(x0,y0)
 			end
 		end
 	end
+	for i,col in ipairs(tokpiles) do
+		if #col == 0 then
+			drawSpace((i*18)+30, 10)
+		else 
+			for j,num in ipairs(col) do
+			 x = (i * 18) + x0
+				y = (j * 6) + y0
+				drawCard(x,y,num)
+			end
+		end
+	end
+	for i,col in ipairs(endpiles) do
+		if #col == 0 then
+			drawSpace((i*18)+102, 10)
+		else 
+			for j,num in ipairs(col) do
+			 x = (i * 18) + x0
+				y = (j * 6) + y0
+				drawCard(x,y,num)
+			end
+		end
+	end
 end
 
 function drawDrag(x,y)
 	if #drag > 0 then
 		for i,c in pairs(drag) do
 			drawCard(x,y,c)
+			y = y + 6
 		end
 	end
 end
 
+function drawButtons()
+	spr(btn_red,102,10,2)
+	spr(btn_red+1,110,10,2)
+	spr(btn_grn,102,18,2)
+	spr(btn_grn+1,110,18,2)
+	spr(btn_blk,102,26,2)
+	spr(btn_blk+1,110,26,2)
+end
+
 function SCN(scnline)
+end
+
+function draw()
 	cls(12)
+	drawButtons()
 	drawCards(30,30)
 	drawDrag(mx-lx,my-ly)
 end
+
+-- Functions to move cards between piles
+function moveHandToPile(pile)
+	while drag[1] ~= nil do
+		table.insert(pile,drag[1])
+		table.remove(drag,1)
+	end
+end
+
+function moveCardToPile(pile0,n,pile1)
+	if pile0[n] == nil then
+		return
+	end
+	table.insert(pile1,pile0[n])
+	table.remove(pile0,n)
+end
+
+function moveStackToHand(pile, n)
+	while pile[n] ~= nil do
+		table.insert(drag,pile[n])
+		table.remove(pile,n)
+	end
+end
+----------------------------------------
 
 function init()
 
@@ -119,37 +198,52 @@ init()
 
 function TIC()
 	mx,my,md = mouse()
+	local lpiles = piles
 	-- DRAG
 	if hold == false and md then
 		hold = true
 		c = math.floor((mx - 30)/18)
-		r = math.floor((my - 30)/6)
+		-- get index of last card in pile
+		if lpiles[c] ~= nil then 
+			lc = #lpiles[c]
+		else
+			lc = 300
+		end
+		maxy = 36 + ((lc - 1) * 6) + 23
+		miny = 36 + ((lc - 1) * 6)
+		-- check if dragging last card
+		if my > miny and my < maxy then
+			r = lc
+		else
+			r = math.floor((my - 30)/6)
+		end
 		if isDraggable(c,r) then
 			lx = mx - (c*18 + 30)
-		 ly = my - (r*6 + 30)
-		 table.insert(drag,piles[c][r])
-			table.remove(piles[c],r)
+			ly = my - (r*6 + 30)
+			while(lpiles[c][r] ~= nil) 
+			do 
+				table.insert(drag,lpiles[c][r])
+				table.remove(lpiles[c],r)
+			end
 			orig = c
 		end
 	end
 	-- DROP
- if hold == true then
-	 if md == false then
-		 c = math.floor((mx - 30)/18)
-		 r = math.floor((my - 30)/6)
-			if piles[c]~=nil then
-				if isOrdered(piles[c][#piles[c]],drag[1]) then
-					table.insert(piles[c], drag[1])
-					table.remove(drag)
-				elseif #drag >0 then
-				 table.insert(piles[orig], drag[1])
-					table.remove(drag)
+	if hold == true then
+		if md == false then
+			c = math.floor((mx - 30)/18)
+			r = math.floor((my - 30)/6)
+			if lpiles[c]~=nil then
+				if isOrdered(lpiles[c][#lpiles[c]],drag[1]) then
+					moveHandToPile(lpiles[c])
+				elseif #drag > 0 then
+					moveHandToPile(lpiles[orig])
 				end
 			elseif #drag > 0 then
-			 table.insert(piles[orig], drag[1])
-				table.remove(drag)
+				moveHandToPile(lpiles[orig])
 			end
 			hold = false
 		end
 	end
+	draw()
 end
