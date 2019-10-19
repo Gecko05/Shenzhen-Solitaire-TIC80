10e70d05be7c19d25d0f916ca79e0ac922eb813a
Jaime Centeno <jacacent@gmail.com>
2019-10-19 17:21:37 -0500

    Buttons and flower functionality

--- shenzhen.lua
+++ shenzhen.lua
@@ -4,34 +4,38 @@
 -- script: lua
 -- input:  mouse
 -- specials: 31 41 51
-piles = {{31,1,2,3,4,41},
+local piles = {{31,1,2,3,4,41},
         {5,6,7,8,9,41,41},
         {11,12,13,14,51,51},
         {15,16,17,18,19,51},
         {21,22,23,24,25,51},
         {27,28,31},
-        {26,41,41,31},
-        {51,31,31,61}}
+        {26,41,31},
+        {31,31,61}}
 
-endpiles = {		{},
+local endpiles = {		{},
 			        {},
 			        {}
 			        }
 
-tokpiles = {		{},
+local tokpiles = {		{},
 			        {},
 			        {}
 		 	        }
+local flowerPile = {}
 -- 	black, green, red
-tokens = {0, 0, 0}
-drag = {}
+local tokenBtns = {0, 0, 0}
+local drag = {}
 local cursor = {x = 0,y = 0,c = 0,hold = false}
 local origin = {x = 0,y = 0,col = 0,isTok = 0}
 local btn_red = 178
 local btn_blk = 180
 local btn_grn = 182
+local animContext = {x0 = 0,y0 = 0,x1 = 0,y1 = 0,xd = 0,yd = 0}
+local animationQueue = {}
 
-local animContext = {first = 0,move = 0,params = {0,0,0},data = {},card = 0}
+------------------------------ D R A W I N G -----------------------------
+
 -- Draw a blank card
 function drawBlnkCard(x, y)
 	spr(0, x,  y,   2)
@@ -57,69 +61,38 @@ function drawSpace(x0,y0)
 	spr(36,x0+0,y0+16,2)
 	spr(37,x0+8,y0+16,2)
 end
--- Gets the number of the first empty token pile
-function getEmptyTokPile()
-	local next = next
-	for i,p in ipairs(tokpiles) do
-		if next(tokpiles[i]) == nil then 
-			return i
-		end 
-	end 
-	return nil
-end 
 
--- Check if cards have different color
-function isDifColor(num0, num1)
-	if num0 ~= nil and num1 ~= nil then
-		res0 = math.floor(num0/10)
-		res1 = math.floor(num1/10)
-		if res0 == res1 then
-			return false
-		end
-		return true
-	end
-	return false
+--Draw blank flower pile
+function drawFlowerSpace(x0,y0)
+	spr(6, x0,  y0,   2)
+	spr(7, x0+8,y0,   2)
+	spr(22,x0+0,y0+8, 2)
+	spr(23,x0+8,y0+8, 2)
+	spr(38,x0+0,y0+16,2)
+	spr(39,x0+8,y0+16,2)
 end
--- Check if 1st card is bigger than 2nd one
-function isOrdered(num0,num1)
-	if num0 == nil then
-		return true
-	end
-	if isDifColor(num0,num1) then
-		n0 = num0 % 10
-		n1 = num1 % 10
-		if n0 - n1 == 1 then
-			return true
-		end
-	end
-	return false
-end
--- Check if the card is draggable
-function isDraggable(c,r)
- if piles[c] == nil or piles[c][r] == nil then
-		return false
-	end
-	n = 1
-	while piles[c][r+n]~=nil do
-		num = piles[c][r+n]
-		if isOrdered(piles[c][r+n-1],num)==false  then
-			return false
-		end
-		n = n + 1
-	end
-	return true
+
+function drawPiledCard(x, y)
+	spr(2, x,  y,   2)
+	spr(3, x+8,y,   2)
+	spr(18,x+0,y+8, 2)
+	spr(19,x+8,y+8, 2)
+	spr(34,x+0,y+16,2)
+	spr(35,x+8,y+16,2)
 end
 
 function drawTokenPiles()
 	-- Draw token piles
 	for i,col in ipairs(tokpiles) do
-		if #col == 0 then
+		if #col <= 0 then
 			drawSpace((i*18) + 30, 10)
-		else 
+		elseif #col < 4 then
 			for j,num in ipairs(col) do
 				x = (i * 18) + 30
 				drawCard(x,10,num)
 			end
+		else
+			drawPiledCard((i*18) + 30, 10)
 		end
 	end
 end
@@ -139,14 +112,22 @@ function drawNormalPiles()
 	end
 end
 
+function drawFlowerPile()
+	if #flowerPile == 0 then
+		drawFlowerSpace(120, 10)
+	else
+		drawCard(120,10,61)
+	end
+end
+
 function drawEndPiles()
 	-- Draw end piles
 	for i,col in ipairs(endpiles) do
 		if #col == 0 then
-			drawSpace((i*18) + 102, 10)
+			drawSpace((i*18) + 120, 10)
 		else 
 			for j,num in ipairs(col) do
-				x = (i * 18) + 102
+				x = (i * 18) + 120
 				drawCard(x,10,num)
 			end
 		end
@@ -158,6 +139,7 @@ function drawCards()
 	drawNormalPiles()
 	drawTokenPiles()
 	drawEndPiles()
+	drawFlowerPile()
 	drawDrag(cursor.x - origin.x,cursor.y - origin.y)
 end
 
@@ -171,21 +153,67 @@ function drawDrag(x,y)
 end
 
 function drawButtons()
-	spr(btn_blk-(tokens[1]),102,10,2)
-	spr(btn_blk+1-(tokens[1]),110,10,2)
-	spr(btn_grn-(tokens[2]),102,18,2)
-	spr(btn_grn+1-(tokens[2]),110,18,2)
-	spr(btn_red-(tokens[3]),102,26,2)
-	spr(btn_red+1-(tokens[3]),110,26,2)
+	spr(btn_blk-(tokenBtns[1]),102,10,2)
+	spr(btn_blk+1-(tokenBtns[1]),110,10,2)
+	spr(btn_grn-(tokenBtns[2]),102,18,2)
+	spr(btn_grn+1-(tokenBtns[2]),110,18,2)
+	spr(btn_red-(tokenBtns[3]),102,26,2)
+	spr(btn_red+1-(tokenBtns[3]),110,26,2)
 end
 
-function draw()
-	cls(12)
-	drawButtons()
-	drawCards()
+--------------------------- C A R D   P L A C I N G ----------------------
+
+function getEmptyTokPile()
+	local next = next
+	for i,p in ipairs(tokpiles) do
+		if next(tokpiles[i]) == nil then 
+			return i
+		end 
+	end 
+	return nil
+end 
+
+function isDifColor(num0, num1)
+	if num0 ~= nil and num1 ~= nil then
+		res0 = math.floor(num0/10)
+		res1 = math.floor(num1/10)
+		if res0 == res1 then
+			return false
+		end
+		return true
+	end
+	return false
 end
 
--- Functions to move cards between piles
+function isOrdered(num0,num1)
+	if num0 == nil then
+		return true
+	end
+	if isDifColor(num0,num1) then
+		n0 = num0 % 10
+		n1 = num1 % 10
+		if n0 - n1 == 1 then
+			return true
+		end
+	end
+	return false
+end
+
+function isDraggable(c,r)
+ if piles[c] == nil or piles[c][r] == nil then
+		return false
+	end
+	n = 1
+	while piles[c][r+n]~=nil do
+		num = piles[c][r+n]
+		if isOrdered(piles[c][r+n-1],num)==false  then
+			return false
+		end
+		n = n + 1
+	end
+	return true
+end
+
 function moveHandToPile(pile)
 	while drag[1] ~= nil do
 		table.insert(pile,drag[1])
@@ -205,83 +233,6 @@ function moveStackToHand(pile,n)
 	while pile[n] ~= nil do
 		table.insert(drag,pile[n])
 		table.remove(pile,n)
-	end
-end
-
-function getAnimParams(cardParams)
-	local x0 = cardParams[1]*18 + 30 -- pile0n
-	local y0 = cardParams[4]*6 + 30 -- cardnum
-	local x1 = 0
-	if cardParams[3] == 1 then 
-		x1 = cardParams[2]*18 + 30 -- pile1n
-	elseif cardParams[3] == 2 then
-		x1 = cardParams[2]*18 + 102 -- pile1n
-	end
-	local y1 = 10
-	local xd = (x0 - x1) / 5
-	local yd = (y0 - y1) / 5
-	return {x0,y0,x1,y1,xd,yd}
-end
-
-function animateCards()
-	-- variable params hold the data of the card to move with animation 
-	-- params = {originPileNum,
-	--			 cardNum,
-	--  		 destPileNum,
-	--			 TokPile?,
-	--			 } 
-	-- Start of animation
-	local anim = animContext
-	local params = anim.params
-	local data = anim.data
-	if anim.first == 0 then 
-		anim.data = getAnimParams(params)
-		anim.card = piles[params[1]][params[4]]
-		table.remove(piles[params[1]], params[4])
-		anim.first = anim.first + 1
-	else -- Animation "cycle"
-		if anim.first < 5 then 
-			data[1] = data[1] -	data[5]
-			data[2] = data[2] - data[6]
-			drawCard(data[1],data[2],data) -- draw the anim card
-			anim.first = anim.first + 1
-		else
-			if params[3] == 1 then 
-				table.insert(tokpiles[params[2]],anim.card)
-			elseif params[3] == 2 then
-				table.insert(endpiles[params[2]],anim.card)
-			end
-			anim.move = 0
-		end 
-	end
-end
-
-function updateButtons()
-	local valSum = {0,0,0,0}
-	-- Check for uncovered tokens in normal piles
-	for k,vpile in pairs(piles) do
-		if #vpile > 0 then 
-			compVal = (vpile[#vpile] - 1)/10 - 2
-			if valSum[compVal] ~= nil then
-				valSum[compVal] = valSum[compVal] + 1
-			end 
-		end
-	end
-	-- Check for uncovered tokens in token piles
-	for k,vpile in pairs(tokpiles) do
-		if #vpile > 0 then 
-			compVal = (vpile[#vpile] - 1)/10 - 2
-			if valSum[compVal] ~= nil then
-				valSum[compVal] = valSum[compVal] + 1
-			end 
-		end 
-	end
-	-- Check the uncovered cards for every token to
-	-- change the token buttons
-	for i,v in ipairs(valSum) do
-		if v == 4 and getEmptyTokPile() ~= nil then
-			tokens[i] = 16
-		end 
 	end
 end
 
@@ -310,39 +261,247 @@ function getSelCardPos()
 	return selectedCol,selectedCard
 end
 
-function isDraggingTokPiles()
-	return cursor.y > 10 and cursor.y < 34 and tokpiles[col] ~= nil
+function isDraggingTokPiles(col)
+	return (cursor.y > 10 and cursor.y < 34 
+			and tokpiles[col] ~= nil and #tokpiles[col] == 1)
 end
 
-function update() 
+function isTokenDropAvailable(col)
+	return (cursor.y > 10 and cursor.y < 34 and #drag == 1 
+			and tokpiles[col]~=nil and isOrdered(tokpiles[col][1],drag[1]))
+end
+
+function dragCardFromTokPiles(col)
+	origin.x = cursor.x - (col*18 + 30)
+	origin.y = cursor.y - (10)
+	table.insert(drag,tokpiles[col][1])
+	table.remove(tokpiles[col],1)
+	origin.isTok = 1
+    origin.col = col
+end
+
+function dragStack(col,card)
+	origin.x = cursor.x - (col*18 + 30)
+	origin.y = cursor.y - (card*6 + 30)
+	moveStackToHand(piles[col],card)
+	origin.isTok = 0
+	origin.col = col
+end
+
+function checkForFlower()
+	for i,pile in ipairs(piles) do
+		lastCard = #pile
+		if pile[lastCard] == 61 then
+			newAnimation(61,i,1,0,3)
+		end
+	end
+end
+
+-------------------------------- B U T T O N S --------------------------
+
+function isPressingButton()
+	return cursor.y > 10 and cursor.y < 34 and cursor.x > 103 and cursor.x < 117
+end
+
+function isButtonOn(btnNumber)
+	return tokenBtns[btnNumber] == 16
+end
+
+function setButtonState(btnNumber, state)
+	state = 16 * state
+	tokenBtns[btnNumber] = state
+end
+
+function getButtonNum()
+	return (cursor.y - 10)//8 + 1
+end
+
+function getTokenIndex(num)
+	return (num - 1)/10 - 2
+end
+
+function isPileAvailable(num)
+	local token = ((num + 2) * 10) + 1
+	for i,pile in ipairs(tokpiles) do
+		if pile[1] == token then
+			return i
+		end
+	end
+	return getEmptyTokPile()
+end
+
+function updateButtons()
+	local tokReps = {0,0,0,0}
+	-- Check for uncovered tokens in normal piles
+	for i,pile in ipairs(piles) do
+		local frontCard = pile[#pile]
+		if frontCard ~= nil then 
+			toki = getTokenIndex(frontCard)
+			if tokReps[toki] ~= nil then
+				tokReps[toki] = tokReps[toki] + 1
+			end
+		end
+	end
+	-- Check for uncovered tokens in token piles
+	for i,pile in ipairs(tokpiles) do
+		local frontCard = pile[1]
+		if frontCard ~= nil then 
+			toki = getTokenIndex(frontCard)
+			if tokReps[toki] ~= nil then
+				tokReps[toki] = tokReps[toki] + 1
+			end 
+		end 
+	end
+	-- Check the repetitions to activate token buttons
+	for i,reps in ipairs(tokReps) do
+		if reps == 4 and isPileAvailable(i) ~= nil then
+			tokenBtns[i] = 16
+		end 
+	end
+end
+
+----------------------------- A N I M A T I O N S ----------------------
+
+function getAnimParams(params)
+	local x0 = params.orig*18 + 30 -- pile0n
+	pile = piles[params.orig]
+	lastCard = #pile
+	local y0 = nil	-- last card's position in Y
+	if params.pileType == 0 then
+		y0 = lastCard*6 + 30 
+	elseif params.pileType == 1 then
+		y0 = lastCard*6 + 10
+	end
+	local x1 = 0
+	if params.token == 1 then 
+		x1 = params.dest*18 + 30 -- to Token Piles
+	elseif params.token == 2 then
+		x1 = params.dest*18 + 120 -- to End piles
+	elseif params.token == 3 then
+		x1 = 120 -- to Flower pile
+	end
+	local y1 = 10
+	local xd = (x0 - x1) / 10
+	local yd = (y0 - y1) / 10
+	return x0,y0,x1,y1,xd,yd
+end
+
+function newAnimation(card, origPile, destPile, pileType, token)
+	local animation = {card = card, orig = origPile, dest = destPile,
+					   state = 0, pileType = pileType, token = token}
+	table.insert(animationQueue,animation)
+end
+
+function playDrawSound()
+	sfx(1,'G#1',3,0,10,2)
+end
+
+function pileTokens(tokenNum)
+	local tokenCard = ((tokenNum + 2) * 10) + 1
+	local pilePos = nil
+	local cardPositions = {}
+	local tokenPositions = {}
+	-- Check if a token is already in a token pile
+	for i,pile in ipairs(tokpiles) do
+		if pile[1] == tokenCard and pilePos == nil then
+			pilePos = i
+		elseif pile[1] == tokenCard then
+			table.insert(tokenPositions,i)
+		end
+	end
+	-- Get an empty pile for tokens
+	if pilePos == nil then 
+		pilePos = getEmptyTokPile()
+	end
+	if pilePos == nil then
+		return
+	end
+	-- Get the piles where every token is uncovered
+	for i,pile in ipairs(piles) do
+		local lastCard = #pile
+		if #pile == 0 then
+			break
+		end
+		if pile[lastCard] == tokenCard then 
+			table.insert(cardPositions,i)
+		end
+	end
+	for i,pile in ipairs(tokenPositions) do
+		newAnimation(tokenCard,pile,pilePos,1,1)
+	end
+	for i,pile in ipairs(cardPositions) do
+		newAnimation(tokenCard,pile,pilePos,0,1)
+	end
+end
+----------------------------------------------------------------------
+function DRAW()
+	cls(12)
+	drawButtons()
+	drawCards()
+end
+
+function ANIMATE()
+	-- Get current element to animate
+	local anim = animationQueue[1]
+	local context = animContext
+	-- Start of animation
+	if anim.state == 0 then 
+		context.x0,context.y0,context.x1,
+		context.y1,context.xd,context.yd = getAnimParams(anim)
+		local pile = nil
+		if anim.pileType == 0 then
+			pile = piles[anim.orig]
+		elseif anim.pileType == 1 then
+			pile = tokpiles[anim.orig]
+		end
+		lastCard = #pile
+		table.remove(pile, lastCard)
+		anim.state = 1
+		playDrawSound()
+	else -- Animation cycle
+		if anim.state < 10 then
+			local x0 = context.x0
+			local y0 = context.y0
+			local xd = context.xd
+			local yd = context.yd
+			context.x0 = x0 - xd
+			context.y0 = y0 - yd
+			drawCard(context.x0,context.y0,anim.card)
+			anim.state = anim.state + 1
+		else
+			if anim.token == 1 then 
+				table.insert(tokpiles[anim.dest],anim.card)
+			elseif anim.token == 2 then
+				table.insert(endpiles[anim.dest],anim.card)
+			elseif anim.token == 3 then
+				table.insert(flowerPile,anim.card)
+			end
+			-- Remove processed animation from queue
+			table.remove(animationQueue, 1)
+		end
+	end
+end
+
+function UPDATE() 
 	-- Check for unblocked tokens
 	updateButtons()
+	checkForFlower()
 	-- DRAG / Click
 	cursor.x,cursor.y,cursor.c = mouse()
 	if isClicking() then
 		cursor.hold = true
 		local col,card = getSelCardPos()
-		-- Check if dragging from token piles
-		if isDraggingTokPiles() then
-			origin.x = cursor.x - (col*18 + 30)
-			origin.y = cursor.y - (10)
-			table.insert(drag,tokpiles[col][1])
-			table.remove(tokpiles[col],1)
-			origin.isTok = 1
-			origin.col = col
-		-- Check if card is draggable
+		if isDraggingTokPiles(col) then
+			dragCardFromTokPiles(col)
 		elseif isDraggable(col,card) then
-			origin.x = cursor.x - (col*18 + 30)
-			origin.y = cursor.y - (card*6 + 30)
-			moveStackToHand(piles[col],card)
-			origin.isTok = 0
-			origin.col = col
+			dragStack(col,card)
 		end
-		-- check for token buttons
-		if cursor.y > 10 and cursor.y < 34 and cursor.x > 103 and cursor.x < 117 then
-			btnNum = (cursor.y - 10)//8 + 1
-			if tokens[btnNum] == 16 then 
-				tokens[btnNum] = 0
+		-- Check if a button is being clicked
+		if isPressingButton() then
+			local btnNum = getButtonNum()
+			if isButtonOn(btnNum) then 
+				setButtonState(btnNum, 0)
+				pileTokens(btnNum)
 			end
 		end
 	end
@@ -351,13 +510,13 @@ function update()
 		if cursor.c == false then
 			local col = math.floor((cursor.x - 30)/18)
 			--card = math.floor((cursor.y - 30)/6)
-			-- Dropping on token piles
-			if cursor.y > 10 and cursor.y < 34 and #drag == 1 and tokpiles[col]~=nil and isOrdered(tokpiles[col][1],drag[1]) then 
+			if isTokenDropAvailable(col) then 
 				moveHandToPile(tokpiles[col])
 			-- Dropping on a pile 
 			elseif piles[col]~=nil then
 				if isOrdered(piles[col][#piles[col]],drag[1]) then
 					moveHandToPile(piles[col])
+				-- Return to origin if can't drop
 				elseif #drag > 0 then
 					if origin.isTok == 1 then
 						moveHandToPile(tokpiles[origin.col])
@@ -373,24 +532,25 @@ function update()
 					moveHandToPile(piles[origin.col])
 				end
 			end
+			-- End of hold
 			cursor.hold = false
 		end
 	end
 end
-----------------------------------------
+------------------------------------------------------------------------
 
 function init()
-	--music(0,0,23,true)
+	--music(0,0,0,true)
 end
 
 init()
 
 function TIC()
-	if  animContext.move == 0 then 
-		update()
-		draw()
+	if  #animationQueue == 0 then 
+		UPDATE()
+		DRAW()
 	else
-		animateCards()
-		draw()
+		DRAW()
+		ANIMATE()
 	end
 end
